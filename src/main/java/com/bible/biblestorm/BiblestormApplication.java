f7df1f6280550556cd9629a313e009453daa11d7
Anthony-cecconi <anthony.cecconi.91@gmail.com>
2025-02-04 10:39:21 +0100

    start

---
+++ src/main/java/com/bible/biblestorm/BiblestormApplication.java
@@ -0,0 +1,13 @@
+package com.bible.biblestorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblestormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblestormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bible/biblestorm/BiblestormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bible.biblestorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblestormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
